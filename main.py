13af111de12d3a975f7b4dfeb89802afbff415d3
Sanders, Nathan <n.m.sanders@tue.nl>
2025-08-28 16:07:23 +0200

    Changed from list to disk and partition dictonaries to build overview of devices and partions; added Windows Admin password reset support

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import os
 
 from _ped import DiskException
 
-version = 0.1
+version = 0.2
 
 raid_disks = []
-partition_list = []
+disk_dict = {}
+partition_dict = {}
 
 def smart():
     subprocess.run(f'xfce4-terminal --command="/bin/bash -c \\\"/usr/bin/crazy; read -p \\\\\\"Press enter to continue...\\\\\\"\\\""', shell=True)
@@ -39,6 +40,9 @@ def ddrescue(partition_path):
 def erase(partition_path):
     subprocess.run(f'xfce4-terminal --command="/bin/bash -c \\\"./HDcleaner/clean_disk.sh {partition_path};read -p \\\\\\"Press enter to continue...\\\\\\"\\\"""', shell=True)
 
+def windows_admin_reset(partition_path):
+    subprocess.run(f'xfce4-terminal --command="/bin/bash -c \\\"umount -f /mnt; mount -f auto {partition_path}; /usr/bin/chntpw /mnt/Windows/System32;read -p \\\\\\"Press enter to continue...\\\\\\"\\\"""', shell=True)
+
 # Get all possible disk that are used in software raid
 # These need to be excluded because the partition table
 # could not be read.
@@ -57,43 +61,59 @@ for device in parted.getAllDevices():
             disk=parted.newDisk(device)
         except DiskException:
             # Empty disk, add device and continue to next device
-            partition_list.append((device.path, False))
+            disk_dict[device.path] = []
             continue
-
-    # Also add device to list of tuples
-    partition_list.append((device.path, False)) 
-
+    
+    
     if hasattr(disk, 'partitions'):
         for partition in disk.partitions:
-            partition_list.append((partition.path, True))
+            partition_label = subprocess.run('lsblk -no lable ' + partition.path, shell=True, capture_output=True).stdout.decode('UTF-8').strip().lower()
+            partition_fstype = subprocess.run('lsblk -no fstype ' + partition.path, shell=True, capture_output=True).stdout.decode('UTF-8').strip().lower()
+            if partion_label == 'vtoyefi' or 'ventoy' in partition_label:
+                continue # Ventoy USB stick should be skipped
+            else:
+                partition_dict['path'] = partition.path
+            if partition_fstype == 'ntfs' and (partition_label == '' or partion_label == 'windows'):
+                partition_dict['possible_windows_installation'] = True
+                if not hasattr(disk_keys, device.path):
+                    disk_dict[device.path] = []
+                disk_dict[device.path].append(partition_dict)
     else:
-        continue
+        # No partitions on this disk, add device and continue to next device
+        disk_dict[device.path] = []
+        continue # If no partitions are availabe, skip to next
 
 
 window = tk.Tk()
 window.title(f'RescueDiskUtil V{version}')
 
 button = tk.Button(window, text="S.M.A.R.T", command=lambda: smart())
-button.grid(row=0, column=0)
+button.grid(row=0, column=3)
 button = tk.Button(window, text="Clone", command=lambda: clonezilla())
-button.grid(row=0, column=1)
+button.grid(row=0, column=4)
 
 i=1
-for partition_path, is_partition in partition_list:
-    label = tk.Label(window, text=partition_path)
+for disk_path in disk_dict.keys():
+    label = tk.Label(window, text=disk_path)
     label.grid(row=i, column=0)
-    if is_partition:
-        button = tk.Button(window, text="mount", command=lambda path=partition_path: mount(path))
-        button.grid(row=i, column=2)
-    else:
-        button = tk.Button(window, text="gparted", command=lambda path=partition_path: gparted(path))
-        button.grid(row=i, column=2)
+    button = tk.Button(window, text="gparted", command=lambda path=disk_path: gparted(path))
+    button.grid(row=i, column=2)
     button = tk.Button(window, text="ddrescue", command=lambda path=partition_path: ddrescue(path))
     button.grid(row=i, column=3)
-    # Check if submodule is checked-out, otherwise HDcleaner can not be used
-    if os.path.isdir('./HDcleaner/') and not is_partition:
-        button = tk.Button(window, text="erase", command=lambda path=partition_path: erase(path))
-        button.grid(row=i, column=4)
-    i+=1
+    for partion_object in disk_dict[disk][0]:
+        partition_path = partition_ojbect['path']
+        label = tk.Label(window, text=partition_path)
+        label.grid(row=i, column=0)
+        button = tk.Button(window, text="mount", command=lambda path=partition_path: mount(path))
+        button.grid(row=i, column=2)
+        if partition_obect['possible_windows_installation']:
+            button = tk.Button(window, text="Admin reset", command=lambda path=partition_path: windows_admin_reset(path))
+            button.grid(row=i, column=3)
+        # Check if submodule is checked-out, otherwise HDcleaner can not be used
+        if os.path.isdir('./HDcleaner/') and not is_partition:
+            button = tk.Button(window, text="erase", command=lambda path=partition_path: erase(path))
+            button.grid(row=i, column=4)
+        i+=1
 
+# Start the GUI
 window.mainloop()
